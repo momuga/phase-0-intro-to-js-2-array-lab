d1d45e3fe5f21ff70c5a8cf251b73f86a1fca10c
Mark <mark.omuga@student.moringaschool.com>
2023-10-25 14:59:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution here!
+
+	const cats = [];
+
+	cats[0] = "Milo";
+	cats[1] = "Otis";
+	cats[2] = "Garfield";
+
+	cats.shift("Garfield");
+
+	cats.unshift("Garfield");
+
+	cats.shift("Milo");
+
+	cats.unshift("Milo");
+
+	cats.push("Ralph");
+
+	cats.push("Broom");
+
+	cats.unshift("Bob");
+
+	cats.shift("Bob");
+
+	cats.unshift("Arnold");
+
+	cats.shift("Arnold");
